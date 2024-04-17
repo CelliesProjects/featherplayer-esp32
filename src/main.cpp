2d1bbf79cad732a4ca4afda36e3bed8cf2e6b9dd
Cellie <CelliesProjects@users.noreply.github.com>
2024-04-17 20:59:53 +0200

    Webserver and websocket running

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 // #include <FS.h>
 // #include <SD.h>
 
+#include "icons.h"
+#include "percentEncode.h"
 #include "tfttask.h"
 #include "playertask.h"
 #include "servertask.h"
@@ -70,7 +72,7 @@ void setup()
     BaseType_t taskResult = xTaskCreatePinnedToCore(
         tftTask,
         "tftTask",
-        4000,
+        2700,
         NULL,
         (tskIDLE_PRIORITY + 2) | portPRIVILEGE_BIT,
         NULL,
@@ -151,6 +153,24 @@ void setup()
         while (1)
             delay(100);
     }
+
+    taskResult = xTaskCreatePinnedToCore(
+        serverTask,
+        "serverTask",
+        4000,
+        NULL,
+        tskIDLE_PRIORITY + 1,
+        NULL,
+        PRO_CPU_NUM);
+
+    if (taskResult != pdPASS)
+    {
+        log_e("FATAL error! Could not create serverTask. System HALTED!");
+        while (1)
+            delay(100);
+    }
+
+    vTaskDelete(NULL);
 }
 
 void loop()
