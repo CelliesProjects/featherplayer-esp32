360ee97b76cc1bcb64ba6461b7ac0e537d716214
Cellie <CelliesProjects@users.noreply.github.com>
2025-03-10 16:07:06 +0100

    dont lock on an empty list

--- src/playList.cpp
+++ src/playList.cpp
@@ -6,37 +6,42 @@ String playList_t::toString()
     s.concat(currentItem());
     s.concat('\n');
 
-    ScopedMutex lock(listMutex);
+    if (!list.size())
+        return s;
 
-    for (const auto &item : list)
     {
-        if (item.type < 0 || item.type >= sizeof(typeStr) / sizeof(typeStr[0]))
+        ScopedMutex lock(listMutex);
+
+        for (const auto &item : list)
         {
-            log_e("ERROR! Playlist item has an invalid type: %d", item.type);
-            continue;
-        }
+            if (item.type < 0 || item.type >= sizeof(typeStr) / sizeof(typeStr[0]))
+            {
+                log_e("ERROR! Playlist item has an invalid type: %d", item.type);
+                continue;
+            }
 
-        switch (item.type)
-        {
-        case HTTP_FILE:
-            s.concat(item.url.substring(item.url.lastIndexOf("/") + 1) + "\n" + typeStr[item.type] + "\n");
-            break;
+            switch (item.type)
+            {
+            case HTTP_FILE:
+                s.concat(item.url.substring(item.url.lastIndexOf("/") + 1) + "\n" + typeStr[item.type] + "\n");
+                break;
 
-        case HTTP_PRESET:
-            if (item.index < NUMBER_OF_PRESETS)
-                s.concat(preset[item.index].name + "\n" + typeStr[item.type] + "\n");
-            else
-                log_e("ERROR! Invalid preset index: %u", item.index);
-            break;
+            case HTTP_PRESET:
+                if (item.index < NUMBER_OF_PRESETS)
+                    s.concat(preset[item.index].name + "\n" + typeStr[item.type] + "\n");
+                else
+                    log_e("ERROR! Invalid preset index: %u", item.index);
+                break;
 
-        case HTTP_FOUND:
-        case HTTP_FAVORITE:
-            s.concat(item.name + "\n" + typeStr[item.type] + "\n");
-            break;
+            case HTTP_FOUND:
+            case HTTP_FAVORITE:
+                s.concat(item.name + "\n" + typeStr[item.type] + "\n");
+                break;
 
-        default:
-            log_e("ERROR! Unhandled item type: %d", item.type);
-            break;
+            default:
+                log_e("ERROR! Unhandled item type: %d", item.type);
+                break;
+            }
         }
     }
     return s;
